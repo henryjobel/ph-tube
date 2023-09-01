c6f898872593b719cbf993b7386b9116540654ad
henryjobel <jhobelsaha@gmail.com>
2023-09-02 03:15:13 +0600

    try to fixed the error

--- javascript/test.js
+++ javascript/test.js
@@ -2,14 +2,11 @@ const allCategory = async () => {
     const response = await fetch ("https://openapi.programming-hero.com/api/videos/categories");
 
     const data = await response.json();
-    console.log(data)
+    // console.log(data)
 
     const tabContainer = document.getElementById("tab-container");
-    tabContainer.innerHTML = "";
-    
     data.data.forEach(category => {
         const div = document.createElement("div")
-        
         div.innerHTML =`
         <a onclick="allLoadVideos('${category.category_id}')"
             class="tab hover:bg-red-500 hover:text-white"
@@ -57,39 +54,6 @@ const allLoadVideos = async (categoryId) => {
     });
     
 }
-const noVideoshow = async (categoryId) => {
-    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
-    const data = await response.json();
-    console.log(categoryId);
 
-    const heroContainer = document.getElementById("hero-container");
-    heroContainer.innerHTML = "";
-
-    if (data.data.length === 0) {
-        
-        const div = document.createElement('div');
-        div.innerHTML = `
-            <div class="hero min-h-screen bg-base-200">
-                <div class="hero-content text-center">
-                    <div class="max-w-md">
-                        <h1 class="text-5xl font-bold">No Videos Found</h1>
-                        <p class="py-6">Sorry, there are no videos available in this category.</p>
-                        <button class="btn btn-primary">Return to Categories</button>
-                    </div>
-                </div>
-            </div>
-        `;
-        heroContainer.appendChild(div);
-    } else {
-        
-        data.data.forEach(video => {
-            console.log(video);
-            
-        });
-    }
-}
-
-
-noVideoshow("1005");
 allCategory();
-allLoadVideos("1000");
+allLoadVideos("1000")
